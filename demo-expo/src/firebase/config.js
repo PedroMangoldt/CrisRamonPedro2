9110940616839cb1f22694c795b3b5a1abfdbf41
ccamdessus <ccamdessus@udesa.edu.ar>
2025-11-03 10:08:45 -0300

    implementacion de firebase

---
+++ demo-expo/src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHJBmjqCOt8QgiH7iX8naGeprcO23iIaM",
+  authDomain: "crisramonpedro.firebaseapp.com",
+  projectId: "crisramonpedro",
+  storageBucket: "crisramonpedro.firebasestorage.app",
+  messagingSenderId: "421007739701",
+  appId: "1:421007739701:web:b3803df9c336340032da7c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
