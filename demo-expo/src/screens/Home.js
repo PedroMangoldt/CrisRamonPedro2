f0fff3992687180bc0ae3486ce9a2c90b10d1443
monchobung <bunger@udesa.edu.ar>
2025-11-10 20:47:26 -0300

    logo gymsocial

--- demo-expo/src/screens/Home.js
+++ demo-expo/src/screens/Home.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, StyleSheet} from 'react-native';
+import { View, Text, FlatList, StyleSheet, Image } from 'react-native';
 import { db, auth } from '../firebase/config';
 import Posteos from '../components/Posteos';
 
-
- class Home extends Component {
+class Home extends Component {
   constructor(props){ 
     super(props);
     this.state = {
@@ -14,32 +13,45 @@ import Posteos from '../components/Posteos';
   }
 
   componentDidMount(){
-  db.collection('posts')
-  .orderBy('createdAt', 'desc')
-  .onSnapshot(
-    docs =>{
-      let posts = [];
-        docs.forEach( doc => {
+    db.collection('posts')
+    .orderBy('createdAt', 'desc')
+    .onSnapshot(
+      docs =>{
+        let posts = [];
+        docs.forEach(doc => {
           posts.push({
             id: doc.id,
             data: doc.data()
           })
         })
-          this.setState({
-            posteos: posts,
-            loading: false,
-          })
-        
-    }
-  )}
+        this.setState({
+          posteos: posts,
+          loading: false,
+        })
+      }
+    )
+  }
 
   render() {
     return (
       <View style={styles.flatlist}>
-        <Text style={styles.sectionTitle}>Home</Text>
+
+        <Image 
+          style={styles.logo}
+          source={require('../../assets/logogym.png')} 
+          resizeMode='contain'
+        />
+
         {this.state.loading ?
-        <Text>Cargando...</Text> :
-        <FlatList data={this.state.posteos} keyExtractor={ item => item.id} renderItem={({item}) => <Posteos posteo={item} navigation={this.props.navigation}/>}></FlatList>}
+          <Text>Cargando...</Text> :
+          <FlatList 
+            data={this.state.posteos}
+            keyExtractor={ item => item.id }
+            renderItem={({item}) => 
+              <Posteos posteo={item} navigation={this.props.navigation}/>
+            }
+          />
+        }
       </View>
     );
   }
@@ -47,18 +59,26 @@ import Posteos from '../components/Posteos';
 
 const styles = StyleSheet.create({
   flatlist: {
-     width: '100%',
-     flex: 1,
-     backgroundColor: '#9EC9FF'
+    width: '100%',
+    flex: 1,
+    backgroundColor: '#9EC9FF'
+  },
+
+  logo:{
+    height: 100,
+    marginTop: 20,
+    marginBottom: 10,
+    alignSelf: 'center',
+    width: '100%'
   },
 
   sectionTitle: {
-   fontSize: 22,
-   fontWeight: '700',
-   marginBottom: 12,
-   color: '#111',
-   paddingTop: 20,
-   textAlign: "center"
+    fontSize: 22,
+    fontWeight: '700',
+    marginBottom: 12,
+    color: '#111',
+    paddingTop: 10,
+    textAlign: "center"
   },
 })
 
